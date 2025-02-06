1b768b357d50369ed39a1b67b2856ca1ea9d0e1f
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-06 08:08:49 -0500

    Refactor Kafka message Consumer and Handler registration.

--- atlas.com/invites/invite/consumer.go
+++ atlas.com/invites/invite/consumer.go
@@ -7,18 +7,26 @@ import (
 	"github.com/Chronicle20/atlas-kafka/handler"
 	"github.com/Chronicle20/atlas-kafka/message"
 	"github.com/Chronicle20/atlas-kafka/topic"
+	"github.com/Chronicle20/atlas-model/model"
 	"github.com/sirupsen/logrus"
 )
 
-func CommandConsumer(l logrus.FieldLogger) func(groupId string) consumer.Config {
-	return func(groupId string) consumer.Config {
-		return consumer2.NewConfig(l)("invite_command")(EnvCommandTopic)(groupId)
+func InitConsumers(l logrus.FieldLogger) func(func(config consumer.Config, decorators ...model.Decorator[consumer.Config])) func(consumerGroupId string) {
+	return func(rf func(config consumer.Config, decorators ...model.Decorator[consumer.Config])) func(consumerGroupId string) {
+		return func(consumerGroupId string) {
+			rf(consumer2.NewConfig(l)("invite_command")(EnvCommandTopic)(consumerGroupId), consumer.SetHeaderParsers(consumer.SpanHeaderParser, consumer.TenantHeaderParser))
+		}
 	}
 }
 
-func CreateCommandRegister(l logrus.FieldLogger) (string, handler.Handler) {
-	t, _ := topic.EnvProvider(l)(EnvCommandTopic)()
-	return t, message.AdaptHandler(message.PersistentConfig(handleCreateCommand))
+func InitHandlers(l logrus.FieldLogger) func(rf func(topic string, handler handler.Handler) (string, error)) {
+	return func(rf func(topic string, handler handler.Handler) (string, error)) {
+		var t string
+		t, _ = topic.EnvProvider(l)(EnvCommandTopic)()
+		_, _ = rf(t, message.AdaptHandler(message.PersistentConfig(handleCreateCommand)))
+		_, _ = rf(t, message.AdaptHandler(message.PersistentConfig(handleAcceptCommand)))
+		_, _ = rf(t, message.AdaptHandler(message.PersistentConfig(handleRejectCommand)))
+	}
 }
 
 func handleCreateCommand(l logrus.FieldLogger, ctx context.Context, c commandEvent[createCommandBody]) {
@@ -28,21 +36,11 @@ func handleCreateCommand(l logrus.FieldLogger, ctx context.Context, c commandEve
 	_ = Create(l)(ctx)(c.Body.ReferenceId, c.WorldId, c.InviteType, c.Body.OriginatorId, c.Body.TargetId)
 }
 
-func AcceptCommandRegister(l logrus.FieldLogger) (string, handler.Handler) {
-	t, _ := topic.EnvProvider(l)(EnvCommandTopic)()
-	return t, message.AdaptHandler(message.PersistentConfig(handleAcceptCommand))
-}
-
 func handleAcceptCommand(l logrus.FieldLogger, ctx context.Context, c commandEvent[acceptCommandBody]) {
 	if c.Type != CommandInviteTypeAccept {
 		return
 	}
 	_ = Accept(l)(ctx)(c.Body.ReferenceId, c.WorldId, c.InviteType, c.Body.TargetId)
-}
-
-func RejectCommandRegister(l logrus.FieldLogger) (string, handler.Handler) {
-	t, _ := topic.EnvProvider(l)(EnvCommandTopic)()
-	return t, message.AdaptHandler(message.PersistentConfig(handleRejectCommand))
 }
 
 func handleRejectCommand(l logrus.FieldLogger, ctx context.Context, c commandEvent[rejectCommandBody]) {
